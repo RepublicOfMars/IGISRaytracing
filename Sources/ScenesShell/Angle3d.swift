7d61f2c788476843fe713a78e1cfc6c908892f9c
William Jackson <william.jackson@student.allenisd.org>
2022-04-04 15:13:25 -0500

    Created Angle3d

--- Sources/ScenesShell/Angle3d.swift
+++ Sources/ScenesShell/Angle3d.swift
@@ -1,3 +1,23 @@
-class Angle3d {
+func absVal(_ n:Double) -> Double {
+    if n < 0 {
+        return -n
+    }
+    return n
+}
 
+class Angle3d {
+    var angleHorizontal : Double
+    var angleVertical : Double
+
+    init(h:Double, v:Double) {
+        angleHorizontal = h
+        angleVertical = v
+    }
+
+    func correctAngle() {
+        while angleHorizontal >= 360.0 {angleHorizontal -= 360.0}
+        while angleVertical >= 360.0 {angleVertical -= 360.0}
+        while angleHorizontal < 0.0 {angleHorizontal += 360.0}
+        while angleVertical < 0.0 {angleVertical += 360.0}
+    }
 }
